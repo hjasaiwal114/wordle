109fa467443a5dfd176e625146f31c86d65cbfa9
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-10-21 18:57:28 +0530

    adding key value to react

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Key from "./key";
 
 const Keyboard = () => {
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+  
   return (
-    <div>Keyboard</div>
+    <div className='Keyboard'>
+      <div className='line1'>
+        {keys1.map((key) => {
+          return <Key keyVal={key} />;
+        })}
+        </div>
+      <div className='line2'>
+        {keys2.map((key) => {
+          return <Key keyVal={key} />;
+        })}
+      </div>
+      <div className='line3'>
+        {keys3.map((key)=> {
+          return <Key keyVal={key} />;
+        })}
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/key.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function key({ keyVal }) {
+  return (
+    <div className='key'>{keyVal}</div>
+  )
+}
+
+export default key
