b53c8a0843b27b3f9b31eeeef9911bad50f7f973
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-10-22 18:30:00 +0530

    done key board know focusing on clrs

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import Key from './Key';
+import React, { useContext, useCallback, useEffect } from "react";
+import { AppContext } from "../App";
+import Key from "./Key" ;
 
+function Keyboard()  {
+  const { onEnter, onDelete, onSelectLetter } = useContext(AppContext);
 
-const Keyboard = () => {
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
   
+  const handelKeyboard = useCallback((event) => {
+    if (event.key === "Enter"){
+      onEnter();
+    } else if (event.key === "Backspace") {
+      onDelete();
+    } else {
+      keys1.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLowerCase()) {
+          onSelectLetter(key);
+        }
+      });
+      keys2.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLocaleLowerCase()) {
+          onSelectLetter(key);
+        }
+      });
+      keys3.forEach((key) => {
+        if (event.key.toLocaleLowerCase() === key.toLocaleLowerCase()) {
+          onSelectLetter(key);
+        }
+      });
+    }
+  })
+
+  useEffect(() => {
+    document.addEventListener("keydown", handelKeyboard);
+
+    return () => {
+      document.removeEventListener("keydown", handelKeyboard);
+    };
+  }, [handelKeyboard]);
+
   return (
-    <div className='Keyboard'>
+    <div className='Keyboard' onKeyDown={handelKeyboard}>
       <div className='line1'>
         {keys1.map((key) => {
           return <Key keyVal={key} />;
