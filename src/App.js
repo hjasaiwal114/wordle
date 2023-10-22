6c36ada720064998a7bca6b203b014897b6a555e
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-10-22 17:45:54 +0530

    seperate function

--- src/App.js
+++ src/App.js
@@ -10,16 +10,37 @@ function App() {
    const [board, setBoard] = useState(boardDefault);
    const [currentAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0});
   
-   const onSelectLetter = () => {
-    
-   }
+   const onSelectLetter = (keyVal) => {
+      if (currentAttempt.letterPos > 4) return;
+      const newBoard = [...board];
+      newBoard[currentAttempt.attempt][currentAttempt.letterPos] = keyVal;
+      setBoard(newBoard);
+      setCurrAttempt({ ...currentAttempt, letterPos: currentAttempt.letterPos + 1});
+   };
   
+   const onDelete = () => {
+    if (currentAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({ ...currentAttempt, letterpos: currentAttempt.letterPos - 1 });
+   };
+
+   const onEnter = () => {
+    if (currentAttempt.letterPos !== 5) return;
+    setCurrAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0});
+   };
+
+
+
    return (
      <div className='App'>
         <nav>
           <h1>Wordle</h1>
         </nav>
-        <AppContext.Provider value={{board, setBoard }}>
+        <AppContext.Provider
+          value={{board, setBoard, currentAttempt, setCurrAttempt }}
+         >
           <div className='game'>
             <Board />
             <Keyboard />
--- src/components/key.js
+++ src/components/key.js
@@ -2,19 +2,16 @@ import React, { useContext } from 'react';
 import { AppContext } from '../App';
 
 function Key({ keyVal , bigKey }) {
-    const { board, setBoard, currAttempt } = useContext(AppContext);
+    const { onDelete, onSelectLetter, onEnter } = useContext(AppContext);
 
     const selectLetter = () => {
-        if (keyVal ==="ENTER") {
-            if (currAttempt.letterPos !== 5) return;
-            setCurrAttempt({attempt: currAttempt.attempt + 1, letterPos: 0});
-        }else {
-          if (currAttempt.letterPos > 4) return;
-          const newBoard = [...board];
-          newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
-          setBoard(newBoard);
-          setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1 });
-        }
+      if (keyVal === "ENTER") {
+        onEnter()
+      } else if (keyVal === "DELETE") {
+          onDelete()
+      } else {
+        onSelectLetter(keyVal);
+      }
     };
   return (
     <div className='key' id={bigKey && "big"} onClick={selectLetter}>
